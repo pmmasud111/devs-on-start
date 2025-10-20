06020c878b77e46da3662107ba59733558ff89e5
pmmasud111 <pmmasud111@gmail.com>
2025-10-21 00:39:55 +0600

    feat: add new component

--- app/components/auth/LoginForm.tsx
+++ app/components/auth/LoginForm.tsx
@@ -1,11 +1,40 @@
-import { TextInput } from "@mantine/core";
+"use client";
+import { useForm } from "@mantine/form";
+import { TextInput, Button } from "@mantine/core";
 
 function LoginForm() {
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: { user: "", password: "" },
+
+    // functions will be used to validate values at corresponding key
+    validate: {
+      user: (value) =>
+        value.length < 2 ? "User must have at least 2 letters" : null,
+      password: (value) =>
+        /^\S+@\S+$/.test(value) ? null : "Invalid password  ",
+    },
+  });
+
   return (
-    <div>
-      <TextInput size="md" placeholder="Input component" />
-    </div>
+    <form onSubmit={form.onSubmit(console.log)}>
+      <TextInput
+        label="Name"
+        placeholder="Name"
+        key={form.key("name")}
+        {...form.getInputProps("name")}
+      />
+      <TextInput
+        mt="sm"
+        label="Email"
+        placeholder="Email"
+        key={form.key("email")}
+        {...form.getInputProps("email")}
+      />
+      <Button type="submit" mt="sm">
+        Submit
+      </Button>
+    </form>
   );
 }
-
 export default LoginForm;
